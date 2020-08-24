574877e174c78957f04cc0a1c01a47e55d20dfe4
Benjamin Brownlie <benjibspam@gmail.com>
2020-08-24 09:29:50 -0500

    Finally got mostly caught back up.
    (fixed role selection issue, lost extra dialog during move)

--- Game.cs
+++ Game.cs
@@ -8,27 +8,53 @@ namespace HelloWorld
     {
         public void Run()
         {
-            Console.WriteLine("Hello, Welcome to my inn. What is your name friend?");
-            string name = Console.ReadLine();
-            Console.WriteLine("Welcome " + name + " ! " + "Please tell me, what are you? ");
-            string role = Console.ReadLine();
             //This variable is used to store health
             float health = 100.0f;
             //This value is used to heal the player
             float healthRegen = 20;
+            //This value is used to store level
             bool maxLevelReached = false;
             int maxLevel = 100;
             int level = 1;
             bool ready = true;
-            Console.WriteLine("Player Name: " + name);
-            Console.WriteLine("Player Health: " + health);
-            Console.WriteLine("Player Level: " + level);
-            Console.WriteLine("Player Role: " + role);
+            //This value is used to store damage
+            int damage = 5;
+
+
+            //Initial conversation
+            Console.WriteLine("Hello, Welcome to my inn. What is your name friend?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name + " ! " + "Please tell me, what are you? ");
+            Console.WriteLine("Press 1. for Hunter");
+            Console.WriteLine("Press 2. for Gatherer");
+            char input = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            //Role selection
+            if (input == '1')
+            {
+                health = health + 50;
+                string role = "Hunter";
+                Console.WriteLine("\nWelcome Hunter " + name);
+                Console.WriteLine("Health: " + health);
+                Console.WriteLine("Level: " + level);
+                Console.WriteLine("Role: " + role);
+            }
+
+            if (input == '2')
+            {
+                string role = "Gatherer";
+                Console.WriteLine("\nWelcome Gatherer " + name);
+                Console.WriteLine("Health: " + health);
+                Console.WriteLine("Level: " + level);
+                Console.WriteLine("Role: " + role);
+
+            }
+
+
             //Create an application that gets the user's name
             //along with an additional input. Print the user's name and input
             //to the console along with their stats. Must use all data types.
-            
-
         }
     }
 }
